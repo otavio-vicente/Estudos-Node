18f88cc55504a0d746c4bc2b7d58105604c28504
Otávio Henrique Vicente <101840512+otavio-vicente@users.noreply.github.com>
2024-05-20 19:32:18 -0300

    Finalização da OT04

---
+++ OT4 - NodeJs/http/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () =>
+console.log('Servidor iniciado na porta 3000')
+);
+
+app.get('/', (req, res) =>
+res.send('<h1 style="color: blue">CRIANDO UM SERVIDOR COM EXPRESS.JS</h1>')
+);
+
+app.get('/sobre', (req, res) =>
+res.send('<h1 style="color: red">PÁGINA SOBRE</h1>')
+);
+
+app.get('/contato', (req, res) =>
+res.send('<h1 style="color: green">PÁGINA CONTATO</h1>')
+);
+
+app.get('/empresa', (req, res) =>
+res.send('<h1 style="color: yellow">PÁGINA EMPRESA</h1>')
+);
